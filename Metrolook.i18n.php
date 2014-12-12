715714ffd83dfa3d60afef61dd8211d6edf1cbdb
paladox2015 <paladox2015@users.noreply.github.com>
2014-12-12 21:25:43 +0000

    Update Metrolook.i18n.php

--- Metrolook.i18n.php
+++ Metrolook.i18n.php
@@ -9,6 +9,23 @@
 
 $messages = array();
 
+/** 
+ * @author Red Winged Duck
+ */
+$messages['be-tarask'] = array(
+	'metrolook-desc' => 'Тэма афармленьня Metrolook для MediaWiki',
+	'metrolook-guest' => 'Госьць',
+);
+
+/** 
+ * @author Metalhead64
+ * @author Purodha
+ */
+$messages['de'] = array(
+	'metrolook-desc' => 'Fügt die Benutzeroberfläche „Metrolook“ zu MediaWiki hinzu',
+	'metrolook-guest' => 'Gast',
+);
+
 /** English
  * @author paladox
  */
@@ -20,8 +37,9 @@ $messages['en'] = array(
 
 /** qqq
  * @author paladox
+ * @author Liuxinyu970226
  */
 $messages['qqq'] = array(
 	'metrolook-desc' => '{{desc|name=Metrolook|url=http://www.mediawiki.org/wiki/Skin:Metrolook}}',
-	'metrolook-guest' => 'Used for descbribing you as a guest editing the wiki.',
+	'metrolook-guest' => 'Used for descbribing you as a guest editing the wiki.\n{{Identical|Guest}}',
 );
